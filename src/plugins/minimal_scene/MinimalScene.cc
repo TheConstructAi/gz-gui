0ab8d53bacada4257afecee8d88396c0d0108ad1
Ruben Alves <rubenanapu@hotmail.com>
2022-05-31 14:02:48 +0000

    Disabling anti-aliasing to get rid of Black Screen on Docker or MVs when using OpenGL 4.5. See https://github.com/gazebosim/gz-sim/issues/1116#issuecomment-954908919

--- src/plugins/minimal_scene/MinimalScene.cc
+++ src/plugins/minimal_scene/MinimalScene.cc
@@ -584,7 +584,7 @@ std::string IgnRenderer::Initialize()
   this->dataPtr->camera->SetFarClipPlane(this->cameraFarClip);
   this->dataPtr->camera->SetImageWidth(this->textureSize.width());
   this->dataPtr->camera->SetImageHeight(this->textureSize.height());
-  this->dataPtr->camera->SetAntiAliasing(8);
+  this->dataPtr->camera->SetAntiAliasing(0);
   this->dataPtr->camera->SetHFOV(this->cameraHFOV);
   // setting the size and calling PreRender should cause the render texture to
   // be rebuilt
